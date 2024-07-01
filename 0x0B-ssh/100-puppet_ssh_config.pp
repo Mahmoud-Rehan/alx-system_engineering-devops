fd50622a3717eea25fbbe439ddb6883301093560
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-30 19:46:56 -0700

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+file {'/etc/ssh/ssh_config':
+  ensure => 'present',
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
